908bdb88c61a9fad287723a0b7d8268da7b5013d
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-09 22:23:38 +0000

    Requirements are now in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "regulations",
-    version = "0.1.0", 
-    license = "public domain", 
-    packages = find_packages()
+    name="regulations",
+    version="2.0.0", 
+    license="public domain", 
+    packages=find_packages(),
+    install_requires=[
+        'django==1.8',
+        'lxml',
+        'requests'
+    ]
 )
