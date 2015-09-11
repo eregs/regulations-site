f036a664beaf932d8497709f073b9b18d3cbffc0
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-11 20:37:46 +0000

    Use CC0 and Public Domain for license

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="regulations",
-    version="2.0.0", 
-    license="public domain", 
+    version="2.0.0",
     packages=find_packages(),
     install_requires=[
         'django==1.8',
         'lxml',
         'requests'
+    ],
+    classifiers=[
+        'License :: Public Domain',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
     ]
 )
