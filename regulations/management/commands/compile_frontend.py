50a24fa5cc7cb4a7e3237dbdd2cb1dd678565c84
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-24 16:16:22 +0000

    Flake8 and split a function into its phases

--- regulations/management/commands/compile_frontend.py
+++ regulations/management/commands/compile_frontend.py
@@ -1,17 +1,15 @@
 import codecs
-import distutils
 import regulations
 import os
 import shutil
 import subprocess
 
-from django.core.management.base import BaseCommand, CommandError
-from django.core.management import call_command
+from django.core.management.base import BaseCommand
 
 """
 This command compiles the frontend for regulations-site after using the Django
-``collectstatic`` command to override specific files, and places the output in a
-directory named ``compiled`` at the root level of the project.
+``collectstatic`` command to override specific files, and places the output in
+a directory named ``compiled`` at the root level of the project.
 
 For example, the atf-eregs project uses ``regulations-site`` as a library and
 overrides the contents of the
@@ -23,22 +21,26 @@ and the results are copied into ``compiled``, which can then be used as the
 static directory for the CSS, font, JavaScript, and image assets.
 
 """
+
+
 class Command(BaseCommand):
     help = 'Build the frontend, including local overrides.'
+    BUILD_DIR = "./frontend_build"
+    TARGET_DIR = "./compiled/regulations"
 
     def find_regulations_directory(self):
         child = regulations.__file__
         child_dir = os.path.split(child)[0]
         return os.path.split(child_dir)[0]
 
-    def handle(self, *args, **options):
-        build_dir = "./frontend_build"
-        target_dir = "./compiled/regulations"
-        for dirpath in (build_dir, target_dir):
+    def remove_dirs(self):
+        """Remove existing output dirs"""
+        for dirpath in (self.BUILD_DIR, self.TARGET_DIR):
             if os.path.exists(dirpath):
                 shutil.rmtree(dirpath)
-        os.environ["TMPDIR"] = build_dir
-        subprocess.call(["python", "manage.py", "collectstatic", "--noinput"])
+
+    def copy_configs(self):
+        """Copy over configs from regulations"""
         regulations_directory = self.find_regulations_directory()
         frontend_files = (
             "package.json",
@@ -47,13 +49,32 @@ class Command(BaseCommand):
             ".eslintrc"
         )
         for f_file in frontend_files:
-            source = "%s/%s" %(regulations_directory, f_file)
-            shutil.copy(source, "%s/" % build_dir)
-        with codecs.open("%s/config.json" % build_dir, "w",
+            source = "%s/%s" % (regulations_directory, f_file)
+            shutil.copy(source, "%s/" % self.BUILD_DIR)
+        with codecs.open("%s/config.json" % self.BUILD_DIR, "w",
                          encoding="utf-8") as f:
             f.write('{"frontEndPath": "static/regulations"}')
-        os.chdir(build_dir)
+
+    def collect_files(self):
+        """Fetch all of the static files from the installed apps -- put them
+        into a specific directory"""
+        os.environ["TMPDIR"] = self.BUILD_DIR
+        subprocess.call(["python", "manage.py", "collectstatic", "--noinput"])
+
+    def build_frontend(self):
+        """Shell out to npm for building the frontend files"""
+        os.chdir(self.BUILD_DIR)
         subprocess.call(["npm", "install", "grunt-cli", "bower"])
         subprocess.call(["npm", "install"])
         os.chdir("..")
-        shutil.copytree("%s/static/regulations" % build_dir, target_dir)
+
+    def cleanup(self):
+        shutil.copytree("%s/static/regulations" % self.BUILD_DIR,
+                        self.TARGET_DIR)
+
+    def handle(self, *args, **options):
+        self.remove_dirs()
+        self.copy_configs()
+        self.collect_files()
+        self.build_frontend()
+        self.cleanup()
