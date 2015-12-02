3f5cebfad302019ae9b5f12391aa8af8b9b4341f
jbarnicle <michael.barnicle@gsa.gov>
2015-12-02 16:54:48 -0600

    fix error in code

--- regulations/static/regulations/js/source/views/main/landing-view.js
+++ regulations/static/regulations/js/source/views/main/landing-view.js
@@ -12,6 +12,7 @@ var LandingView = Backbone.View.extend({
     landingContentKey: 'landing-content',
 
     initialize: function() {
+        console.log('Fraggle ' + Backbone.history.fragment);
         this.setContent();
         this.externalEvents = MainEvents;
         this.externalEvents.on('landing:open', this.render);
@@ -19,18 +20,19 @@ var LandingView = Backbone.View.extend({
     },
 
     setContent: function() {
+        console.log('Fraggle ' + Backbone.history.fragment);
         // Backbone history check here is invalid - would remove that
         // check once the router "landing:set" event was working
-        if ((typeof(Storage) !== "undefined") && 
-            (Backbone.history.fragment.indexOf("/") == -1))
+        if (typeof(Storage) !== "undefined" && 
+            Backbone.history.fragment.indexOf("/") == -1) {
             console.log('setting content');
             localStorage.setItem(this.landingContentKey, $('#content-body').html());
         }
     },
 
     render: function() {
-        if ((typeof(Storage) !== "undefined") && 
-            localstorage.getItem(this.landingContentKey) {
+        if (typeof(Storage) !== "undefined" && 
+            localstorage.getItem(this.landingContentKey)) {
                 // for some reason this.$el is not working
                 $('#content-body').html(localStorage.getItem(this.landingContentKey));
         }
