aeb19b601f4557d1b2abca6090ecec0e2a6b2515
jbarnicle <michael.barnicle@gsa.gov>
2015-12-02 13:56:40 -0600

    landing view for home page

---
+++ regulations/static/regulations/js/source/views/main/landing-view.js
@@ -0,0 +1,40 @@
+'use strict';
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var Router = require('../../router');
+var MainEvents = require('../../events/main-events');
+Backbone.$ = $;
+
+var LandingView = Backbone.View.extend({
+    
+    el: '#content-body',
+    landingContentKey: 'landing-content',
+
+    initialize: function() {
+        this.setContent();
+        this.externalEvents = MainEvents;
+        this.externalEvents.on('landing:open', this.render);
+        this.externalEvents.on('landing:set', this.setContent);
+    },
+
+    setContent: function() {
+        // Backbone history check here is invalid - would remove that
+        // check once the router "landing:set" event was working
+        if ((typeof(Storage) !== "undefined") && 
+            (Backbone.history.fragment.indexOf("/") == -1))
+            console.log('setting content');
+            localStorage.setItem(this.landingContentKey, $('#content-body').html());
+        }
+    },
+
+    render: function() {
+        if ((typeof(Storage) !== "undefined") && 
+            localstorage.getItem(this.landingContentKey) {
+                // for some reason this.$el is not working
+                $('#content-body').html(localStorage.getItem(this.landingContentKey));
+        }
+    }
+});
+
+module.exports = LandingView;
