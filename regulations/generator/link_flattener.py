7c3c0fac58822bcfa7bbd69de5ce46b36f84740c
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2016-01-28 06:51:46 -0500

    Simplify regex using non-greedy qualifier

--- regulations/generator/link_flattener.py
+++ regulations/generator/link_flattener.py
@@ -1,9 +1,10 @@
 import re
 
 # <a> followed by another <a> without any intervening </a>s
+# outer_link - partial outer element up to the inner link
+# inner_content - content of the inner_link
 link_inside_link_regex = re.compile(
-    ur"(?P<outer_link><a ((?!</a>).)*)(<a ((?!</a>).)*>"
-    ur"(?P<internal_content>((?!</a>).)*)</a>)",
+    ur"(?P<outer_link><a ((?!</a>).)*)<a .*?>(?P<inner_content>.*?)</a>",
     re.IGNORECASE | re.DOTALL)
 
 
@@ -11,12 +12,10 @@ def flatten_links(text):
     """
     Fix <a> elements that have embedded <a> elements by
     replacing the internal <a> element with its content.
-    Assumes that the text does not span multiple lines and that
-    the <a> tags are lowercase.
     """
 
     while True:
         text, sub_count = link_inside_link_regex.subn(
-            ur"\g<outer_link>\g<internal_content>", text)
+            ur"\g<outer_link>\g<inner_content>", text)
         if sub_count == 0:
-            return text # Return only when no more subs possible
+            return text
