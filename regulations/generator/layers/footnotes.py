9b1bfda7b21088329c43c36a928084b8180a430e
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2016-02-08 16:17:37 -0500

    Document fucntion behavior and limitations

--- regulations/generator/layers/footnotes.py
+++ regulations/generator/layers/footnotes.py
@@ -12,7 +12,15 @@ class FootnotesLayer(LayerBase):
         self.version = version
 
     def apply_layer(self, text_index):
-        """Return a tuple of 'footnotes' and collection of footnotes"""
+        """
+        Return a tuple of 'footnotes' and collection of footnotes.
+        Footnotes are "collected" from the node and its children.
+        In the layer data, child nodes have their label prefix equal
+        to that of their parent.
+        .. note::
+           This does not handle the case where the same note reference
+           is used in multiple children.
+        """
         footnotes = []
         for label in self.layer.keys():
             if label.startswith(text_index):
