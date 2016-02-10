a3d6aed1aa8cba7e5c9c45daf2d8f920f27d8a68
Joshua Carp <jm.carp@gmail.com>
2016-02-10 13:03:05 -0500

    Use minifyify for sensible javascript source maps.
    Part of https://github.com/18F/atf-eregs/issues/338

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@
 
 module.exports = function(grunt) {
 
+  var env = grunt.file.readJSON('config.json');
+
   grunt.initConfig({
 
     /**
@@ -14,7 +16,7 @@ module.exports = function(grunt) {
      *  Pull in environment-specific vars
      *
      */
-    env: grunt.file.readJSON('config.json'),
+    env: env,
 
     /**
      * https://github.com/gruntjs/grunt-contrib-less
@@ -81,21 +83,21 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          '<%= env.frontEndPath %>/js/built/regulations.js': ['<%= env.frontEndPath %>/js/source/regulations.js']
+          '<%= env.frontEndPath %>/js/built/regulations.min.js': ['<%= env.frontEndPath %>/js/source/regulations.js']
         },
         options: {
           transform: ['browserify-shim'],
           browserifyOptions: {
-            debug: false
-          }
-        }
-      }
-    },
-
-    uglify: {
-      dist: {
-        files: {
-          '<%= env.frontEndPath %>/js/built/regulations.min.js': ['<%= env.frontEndPath %>/js/built/regulations.js']
+            debug: true
+          },
+          plugin: [
+            [function(b) {
+              b.plugin('minifyify', {
+                map: '/static/regulations/js/built/regulations.min.map',
+                output: env.frontEndPath + '/js/built/regulations.min.map'
+              });
+            }]
+          ],
         }
       }
     },
@@ -176,6 +178,6 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['eslint', 'mocha_istanbul', 'nose']);
   grunt.registerTask('test-js', ['eslint', 'mocha_istanbul']);
   grunt.registerTask('build', ['default', 'test-js']);
-  grunt.registerTask('squish', ['browserify', 'uglify', 'less', 'cssmin']);
-  grunt.registerTask('default', ['browserify', 'uglify', 'less', 'cssmin']);
+  grunt.registerTask('squish', ['browserify', 'less', 'cssmin']);
+  grunt.registerTask('default', ['browserify', 'less', 'cssmin']);
 };
