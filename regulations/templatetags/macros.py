8777a60d745b5f73c23e73c326b8f75c599d3078
CM Lubinski <christopher.lubinski@gsa.gov>
2016-02-16 15:04:20 +0000

    Macro for searching. Docs explaining templates

--- regulations/templatetags/macros.py
+++ regulations/templatetags/macros.py
@@ -1,3 +1,4 @@
+"""Macros used to make a few common tasks in templates easier"""
 from django import template
 
 register = template.Library()
@@ -6,3 +7,10 @@ register = template.Library()
 @register.inclusion_tag('regulations/macros/external_link.html')
 def external_link(url, text, classes=""):
     return {"url": url, "text": text, "classes": classes}
+
+
+@register.inclusion_tag('regulations/macros/search_for.html')
+def search_for(terms, reg, version, text=None):
+    if text is None:
+        text = terms
+    return {"terms": terms, "reg": reg, "version": version, "text": text}
