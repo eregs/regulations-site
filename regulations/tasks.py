7d642df7afbcb47fffa9afcad6ab9275d9a5b44a
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2016-03-23 15:20:51 -0400

    Make entire attachment assembly context managed.
    This helps localize the cleanup needed after POST as well as improves
    readability of the code.

--- regulations/tasks.py
+++ regulations/tasks.py
@@ -14,44 +14,61 @@ from django.conf import settings
 
 @shared_task
 def submit_comment(comment):
-    with TemporaryDirectory() as path:
+    with AttachmentSet(comment) as attachments:
         fields = [
             ("comment_on", comment["document_id"]),
             ("general_comment", comment["comment"]),
         ]
-        try:
-            files = [
-                ('uploadedFile', (file_['name'], open(
-                    fetch_file(path, file_['key'], file_['name']), "rb")))
-                for file_ in comment.get('files', [])
-            ]
-            fields.extend(files)
-            data = MultipartEncoder(fields)
-            response = requests.post(
-                settings.REGS_API_URL,
-                data=data,
-                headers={
-                    "Content-Type": data.content_type,
-                    "X-Api-Key": settings.REGS_API_KEY
-                }
-            )
-        finally:
-            for file_ in files:
-                file_[1][1].close()
-
+        fields.extend(attachments)
+        data = MultipartEncoder(fields)
+        response = requests.post(
+            settings.REGS_API_URL,
+            data=data,
+            headers={
+                "Content-Type": data.content_type,
+                "X-Api-Key": settings.REGS_API_KEY
+            }
+        )
         print(response.text)
 
 
 @contextlib.contextmanager
-def TemporaryDirectory(*args, **kwargs):
+def AttachmentSet(comment):
+    '''
+    Assembles a collection of tuples of the form:
+    [
+        ('uploadedFile', ('fileName1', file-object1),
+        ('uploadedFile', ('fileName2', file-object2),
+        ...
+    ]
+    for POSTing as a multipart/form-data upload.
+    comment['files'] is a collection of dicts where for each dict:
+        - dict['key'] specifies the file to be attached from S3
+        - dict['name'] specifies the name under which the file is to be
+          attached.
+    On context exit, the file objects are closed and the locally
+    downloaded files are deleted.
+    '''
     try:
-        path = tempfile.mkdtemp(*args, **kwargs)
-        yield path
+        path = tempfile.mkdtemp()
+        attachments = [
+            ('uploadedFile', (file_['name'],
+                              fetch_file(path, file_['key'], file_['name'])))
+            for file_ in comment.get('files', [])
+        ]
+        yield attachments
     finally:
+        for file_ in attachments:
+            attachments[1][1].close()
         shutil.rmtree(path)
 
 
 def fetch_file(path, key, name):
+    '''
+    Returns a file object corresponding to a local file stored at ``path+name``
+    whose content is downloaded from S3 where it is stored under ``key``
+
+    '''
     session = boto3.Session(
         aws_access_key_id=settings.ATTACHMENT_ACCESS_KEY_ID,
         aws_secret_access_key=settings.ATTACHMENT_SECRET_ACCESS_KEY,
@@ -59,4 +76,4 @@ def fetch_file(path, key, name):
     s3 = session.client('s3')
     dest = os.path.join(path, name)
     s3.download_file(settings.ATTACHMENT_BUCKET, key, dest)
-    return dest
+    return open(dest, "rb")
