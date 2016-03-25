f4a04d62de2e83c146caf5237e72967185560ba2
Joshua Carp <jm.carp@gmail.com>
2016-03-25 10:38:06 -0400

    Add command for S3 CORS configuration.
    To be run on app startup or manually.
    
    [Resolves https://github.com/eregs/notice-and-comment/issues/57]

---
+++ regulations/management/commands/setup_cors.py
@@ -0,0 +1,26 @@
+import boto3
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Set CORS rules on the Notice and Comment attachment bucket'
+
+    def handle(self, *args, **options):
+        session = boto3.Session(
+            aws_access_key_id=settings.ATTACHMENT_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.ATTACHMENT_SECRET_ACCESS_KEY,
+        )
+        s3 = session.client('s3')
+        s3.put_bucket_cors(
+            Bucket=settings.ATTACHMENT_BUCKET,
+            CORSConfiguration={
+                'CORSRules': [
+                    {
+                        'AllowedMethods': ['PUT'],
+                        'AllowedOrigins': ['*'],
+                        'AllowedHeaders': ['*'],
+                    },
+                ],
+            },
+        )
