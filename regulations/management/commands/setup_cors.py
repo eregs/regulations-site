316066b2415861b65d540b822df1b2afea906207
CM Lubinski <christopher.lubinski@gsa.gov>
2016-04-22 15:15:58 +0000

    Add protocol to hosts for CORS

--- regulations/management/commands/setup_cors.py
+++ regulations/management/commands/setup_cors.py
@@ -7,18 +7,23 @@ class Command(BaseCommand):
     help = 'Set CORS rules on the Notice and Comment attachment bucket'
 
     def handle(self, *args, **options):
+        hosts = settings.ALLOWED_HOSTS
+        origins = ['http://' + host for host in hosts]
+        origins = origins + ['https://' + host for host in hosts]
+
         session = boto3.Session(
             aws_access_key_id=settings.ATTACHMENT_ACCESS_KEY_ID,
             aws_secret_access_key=settings.ATTACHMENT_SECRET_ACCESS_KEY,
         )
         s3 = session.client('s3')
+
         s3.put_bucket_cors(
             Bucket=settings.ATTACHMENT_BUCKET,
             CORSConfiguration={
                 'CORSRules': [
                     {
                         'AllowedMethods': ['GET', 'PUT'],
-                        'AllowedOrigins': settings.ALLOWED_HOSTS or ['*'],
+                        'AllowedOrigins': origins or ['*'],
                         'AllowedHeaders': ['*'],
                     },
                 ],
