88a2997c70d0e892522501fe7c763d3470cdcf12
CM Lubinski <christopher.lubinski@gsa.gov>
2016-05-12 16:50:52 +0000

    Comment close in the future

--- local_settings_test.py
+++ local_settings_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import date, timedelta
 
 API_BASE = "http://127.0.0.1:8282/"
 
@@ -59,85 +60,16 @@ CFR_CHANGES = {
         ]
     }
 }
+future = date.today() + timedelta(days=10)
 PREAMBLE_INTRO = {
     "2016_02749": {
-        "tree": {
-            "title": "Preamble introduction",
-            "text": "",
-            "node_type": "preamble_intro",
-            "label": ["2016_02749", "intro"],
-            "children": [
-                {"title": "Summary:",
-                 "text": """The U.S. Environmental Protection Agency (EPA) is
-                 proposing to add a subsurface intrusion (SsI) component to
-                 the Hazard Ranking System (HRS) which is the principal
-                 mechanism that EPA uses to evaluate sites for placement on
-                 the National Priorities List (NPL). The subsurface intrusion
-                 component (this addition) would expand the number of
-                 available options for EPA and state and tribal organizations
-                 performing work on behalf of EPA to evaluate potential
-                 threats to public health from releases of hazardous
-                 substances, pollutants, or contaminants. This addition will
-                 allow an HRS evaluation to directly consider human exposure
-                 to hazardous substances, pollutants, or contaminants that
-                 enter regularly occupied structures through subsurface
-                 intrusion in assessing a site's relative risk, and thus,
-                 enable subsurface intrusion contamination to be evaluated for
-                 placement of sites on the NPL. The agency is not considering
-                 changes to the remainder of the HRS except for minor updates
-                 reflecting changes in terminology.""",
-                 "node_type": "preamble",
-                 "label": ["2016_02749", "intro", "p3"],
-                 "children": []},
-                {"title": "Dates:",
-                 "text": ("Comments must be received on or before April "
-                          "29, 2016."),
-                 "node_type": "preamble",
-                 "label": ["2016_02749", "intro", "p4"],
-                 "children": []},
-                {"title": "Addresses:",
-                 "text": """Submit your comments, identified by Docket ID No.
-                 EPA-HQ-SFUND-2010-1086, to the Federal eRulemaking Portal:
-                 http://www.regulations.gov. Follow the online instructions
-                 for submitting comments. Once submitted, comments cannot be
-                 edited or withdrawn. The EPA may publish any comment received
-                 to its public docket. Do not submit electronically any
-                 information you consider to be Confidential Business
-                 Information (CBI) or other information whose disclosure is
-                 restricted by statute. Multimedia submissions (audio, video,
-                 etc.) must be accompanied by a written comment. The written
-                 comment is considered the official comment and should include
-                 discussion of all points you wish to make. The EPA will
-                 generally not consider comments or comment contents located
-                 outside of the primary submission (i.e. on the Web, cloud, or
-                 other file sharing system). For additional submission
-                 methods, the full EPA public comment policy, information
-                 about CBI or multimedia submissions, and general guidance on
-                 making effective comments, please visit
-                 http://www.epa.gov/dockets/commenting-epa-dockets.""",
-                 "node_type": "preamble",
-                 "label": ["2016_02749", "intro", "p5"],
-                 "children": []},
-                {"title": "For further information contact:",
-                 "text": """Terry Jeng, phone: (703) 603-8852, email:
-                 jeng.terry@epa.gov, Site Assessment and Remedy Decisions
-                 Branch, Assessment and Remediation Division, Office of
-                 Superfund Remediation and Technology Innovation (Mail Code
-                 5204P), U.S. Environmental Protection Agency, 1200
-                 Pennsylvania Avenue NW., Washington, DC 20460; or the
-                 Superfund Hotline, phone (800) 424-9346 or (703) 412-9810 in
-                 the Washington, DC metropolitan area.""",
-                 "node_type": "preamble",
-                 "label": ["2016_02749", "intro", "p6"],
-                 "children": []}
-                ]},
         "meta": {
             "primary_agency": "Environmental Protection Agency",
             "agencies": ["Environmental Protection Agency"],
             "action": "Proposed rule",
             "title": ("Addition of a Subsurface Intrusion Component to the "
                       "Hazard Ranking System"),
-            "comments_close": "2016-04-29",
+            "comments_close": future.isoformat(),
             "publication": "2016-02-29",
             "cfr_parts": [{"title": "40", "parts": ["300"]}],
             "dockets": ["EPA-HQ-SFUND-2010-1086",
