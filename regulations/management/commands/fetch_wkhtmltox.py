68cfa94c9f35a88b629922f12bbf01ff2c3b914b
CM Lubinski <christopher.lubinski@gsa.gov>
2016-06-02 00:34:51 +0000

    Avoid downloading wkhtmltox multiple times
    Downloading the wkhtmltox binary slows down our deploy, often triggering a
    timeout. With this patch, the binary will only be downloaded if it's not part
    of the push bundle. It also adds a more forgiving default for the wkhtmltox
    location.

--- regulations/management/commands/fetch_wkhtmltox.py
+++ regulations/management/commands/fetch_wkhtmltox.py
@@ -1,6 +1,8 @@
 import platform
+import os.path
 import subprocess
 
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 
@@ -13,12 +15,24 @@ BIN_PATH = (
 class Command(BaseCommand):
     help = 'Fetch wkhtmltox binary'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--force', action='store_true', dest='force', default=False,
+            help="Download the binary even if it's present")
+
     def handle(self, **options):
         if platform.system() != 'Linux':
             raise CommandError(
                 'The `wkhtmltox` command only handles linux; to install on '
                 'another platform, see http://wkhtmltopdf.org/downloads.html.'
             )
-        subprocess.check_call([
-            'wget', '-O', BIN_PATH[-1], '/'.join(BIN_PATH)])
-        subprocess.check_call(['tar', 'xvf', BIN_PATH[-1]])
+
+        path = settings.WKHTMLTOPDF_PATH
+        if path and os.path.exists(path) and not options['force']:
+            self.stderr.write(self.style.WARNING(
+                "WKHTMLTOPDF already exists: {}\nUse --force?".format(
+                    settings.WKHTMLTOPDF_PATH)))
+        else:
+            subprocess.check_call([
+                'wget', '-O', BIN_PATH[-1], '/'.join(BIN_PATH)])
+            subprocess.check_call(['tar', 'xvf', BIN_PATH[-1]])
