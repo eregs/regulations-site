a3a301e0e84c1efb78caa700f8a4adabb8e62229
CM Lubinski <cm.lubinski@gmail.com>
2016-07-03 15:43:25 -0400

    Remove frontend build from setup.py
    Resolves #424. See that issue for more detail

--- setup.py
+++ setup.py
@@ -1,40 +1,4 @@
-import os
-from subprocess import call     # nosec  - see usage below
-
-from distutils.command.build_ext import build_ext as _build_ext
-from setuptools import Command, find_packages, setup
-from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
-
-
-class build_frontend(Command):
-    """ A command class to run `frontendbuild.sh` """
-    description = 'build front-end JavaScript and CSS'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        # Safe because: calling our own script
-        call(['./frontendbuild.sh'],    # nosec
-             cwd=os.path.dirname(os.path.abspath(__file__)))
-
-
-class build_ext(_build_ext):
-    """ A build_ext subclass that adds build_frontend """
-    def run(self):
-        self.run_command('build_frontend')
-        _build_ext.run(self)
-
-
-class bdist_egg(_bdist_egg):
-    """ A bdist_egg subclass that runs build_frontend """
-    def run(self):
-        self.run_command('build_frontend')
-        _bdist_egg.run(self)
+from setuptools import find_packages, setup
 
 
 setup(
@@ -51,11 +15,6 @@ setup(
         'six',
         'requests-toolbelt',
     ],
-    cmdclass={
-        'build_frontend': build_frontend,
-        'build_ext': build_ext,
-        'bdist_egg': bdist_egg,
-    },
     classifiers=[
         'License :: Public Domain',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
