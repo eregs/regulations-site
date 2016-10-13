4debf29a44d3ee907a4b2b8013ae1584820a5727
CM Lubinski <christopher.lubinski@gsa.gov>
2016-10-13 17:50:06 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="regulations",
-    version="6.1.0",
+    version="6.2.0",
     packages=find_packages(),
     install_requires=[
         'boto3',
