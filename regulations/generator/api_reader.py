f62dccbe6e888278d45a3be11f33e3daded9c40a
CM Lubinski <cm.lubinski@gmail.com>
2016-11-24 12:16:06 -0500

    Simplify ApiReader._get
    We were encoding the cache key separate from the request. We derive a unique
    key from the request, however, so that's not required. Also resolves a
    mutable-default-arg issue.

--- regulations/generator/api_reader.py
+++ regulations/generator/api_reader.py
@@ -15,12 +15,10 @@ class ApiReader(object):
 
     def all_regulations_versions(self):
         """ Get all versions, for all regulations. """
-        return self._get(['all_regulations_versions'], 'regulation')
+        return self._get('regulation')
 
     def regversions(self, label):
-        return self._get(
-            ['regversions', label],
-            'regulation/%s' % label)
+        return self._get('regulation/{}'.format(label))
 
     def cache_root_and_interps(self, reg_tree, version, is_root=True):
         """We will re-use the root tree at multiple points during page
@@ -49,8 +47,12 @@ class ApiReader(object):
                 self.cache_root_and_interps(regulation, version)
                 return regulation
 
-    def _get(self, cache_key_elements, api_suffix, api_params={}):
+    def _get(self, api_suffix, api_params=None):
         """ Retrieve from the cache whenever possible, or get from the API """
+        if api_params is None:
+            api_params = {}
+        cache_key_elements = api_suffix.split('/') + list(sorted(
+            element for pair in api_params.items() for element in pair))
         cache_key = _cache_key(cache_key_elements)
         cached = self.cache.get(cache_key)
 
@@ -72,39 +74,28 @@ class ApiReader(object):
         else:
             doc_id = '{}/{}'.format(version, root)
         result = self._get(
-            ('layer', layer_name, doc_type, root, str(version)),
             'layer/{}/{}/{}'.format(layer_name, doc_type, doc_id))
         # To remove - also try the old format for CFR layers; the API may not
         # have been updated
         if result is None and doc_type == 'cfr':
             result = self._get(
-                ('layer', layer_name, doc_type, root, str(version)),
                 'layer/{}/{}/{}'.format(layer_name, root, version))
         return result
 
     def diff(self, label, older, newer):
         """ End point for diffs. """
-        return self._get(
-            ['diff', label, older, newer],
-            "diff/%s/%s/%s" % (label, older, newer))
+        return self._get("diff/{}/{}/{}".format(label, older, newer))
 
     def notices(self, part=None):
         """ End point for notice searching. Right now just a list. """
+        params = None
         if part:
-            return self._get(
-                ['notices', part],
-                'notice',
-                {'part': part})
-        else:
-            return self._get(
-                ['notices'],
-                'notice')
+            params = {'part': part}
+        return self._get('notice', params)
 
     def notice(self, fr_document_number):
         """ End point for retrieving a single notice. """
-        return self._get(
-            ['notice', fr_document_number],
-            'notice/%s' % fr_document_number)
+        return self._get('notice/{}'.format(fr_document_number))
 
     def search(self, query, doc_type='cfr', version=None, regulation=None,
                **kwargs):
@@ -119,5 +110,4 @@ class ApiReader(object):
         return self.client.get('/'.join(['search', doc_type]), params)
 
     def preamble(self, doc_number):
-        return self._get(
-            ['preamble', doc_number], 'preamble/{}'.format(doc_number))
+        return self._get('preamble/{}'.format(doc_number))
