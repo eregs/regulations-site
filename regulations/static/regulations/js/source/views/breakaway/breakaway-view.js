joshuaeveleth/regulations-site-1

Respect no-var rule (use let/const)

regulations/static/regulations/js/source/views/breakaway/breakaway-view.js
@@ -1,17 +1,17 @@
 
 
-var $ = require('jquery');
-var _ = require('underscore');
-var Backbone = require('backbone');
-var SxS = require('./sxs-view');
-var Router = require('../../router');
-var BreakawayEvents = require('../../events/breakaway-events');
-var MainEvents = require('../../events/main-events');
-var SidebarEvents = require('../../events/sidebar-events');
+const $ = require('jquery');
+const _ = require('underscore');
+const Backbone = require('backbone');
+const SxS = require('./sxs-view');
+const Router = require('../../router');
+const BreakawayEvents = require('../../events/breakaway-events');
+const MainEvents = require('../../events/main-events');
+const SidebarEvents = require('../../events/sidebar-events');
 
 Backbone.$ = $;
 
-var BreakawayView = Backbone.View.extend({
+const BreakawayView = Backbone.View.extend({
   childViews: {},
 
   initialize: function initialize() {
@@ -19,7 +19,7 @@ var BreakawayView = Backbone.View.extend({
   },
 
   openSxS: function openSxS(givenContext) {
-    var context = $.extend({}, givenContext);
+    const context = $.extend({}, givenContext);
     context.url = context.regParagraph + '/' + context.docNumber + '?from_version=' + context.fromVersion;
 
     this.childViews.sxs = new SxS(context);
@@ -38,5 +38,5 @@ var BreakawayView = Backbone.View.extend({
   },
 });
 
-var breakaway = new BreakawayView();
+const breakaway = new BreakawayView();
 module.exports = breakaway;

regulations/static/regulations/js/source/views/drawer/drawer-view.js
@@ -1,16 +1,16 @@
 
 
-var $ = require('jquery');
-var Backbone = require('backbone');
-var TOCView = require('./toc-view');
-var HistoryView = require('./history-view');
-var SearchView = require('./search-view');
-var DrawerTabs = require('./drawer-tabs-view');
-var DrawerEvents = require('../../events/drawer-events');
+const $ = require('jquery');
+const Backbone = require('backbone');
+const TOCView = require('./toc-view');
+const HistoryView = require('./history-view');
+const SearchView = require('./search-view');
+const DrawerTabs = require('./drawer-tabs-view');
+const DrawerEvents = require('../../events/drawer-events');
 
 Backbone.$ = $;
 
-var DrawerView = Backbone.View.extend({
+const DrawerView = Backbone.View.extend({
   el: '#menu',
 
   initialize: function initialize(options) {
@@ -26,7 +26,7 @@ var DrawerView = Backbone.View.extend({
     this.childViews.search = new SearchView();
     this.childViews['drawer-tabs'] = new DrawerTabs({ forceOpen: options.forceOpen });
 
-    var $tocSecondary = $('#table-of-contents-secondary');
+    const $tocSecondary = $('#table-of-contents-secondary');
     if ($tocSecondary.length) {
       this.childViews['table-of-contents-secondary'] = new TOCView({ el: $tocSecondary });
     }
@@ -46,7 +46,7 @@ var DrawerView = Backbone.View.extend({
 
   // activeId = page type or child view type
   setActivePane: function setActivePane(givenActiveId) {
-    var activeId = givenActiveId;
+    let activeId = givenActiveId;
     if (typeof this.childViews[activeId] === 'undefined') {
       activeId = this.pageTypeMap[activeId];
     }

regulations/static/regulations/js/source/views/drawer/history-view.js
@@ -1,13 +1,13 @@
 
 
-var $ = require('jquery');
-var _ = require('underscore');
-var Backbone = require('backbone');
-var MainEvents = require('../../events/main-events');
+const $ = require('jquery');
+const _ = require('underscore');
+const Backbone = require('backbone');
+const MainEvents = require('../../events/main-events');
 
 Backbone.$ = $;
 
-var HistoryView = Backbone.View.extend({
+const HistoryView = Backbone.View.extend({
 
   el: '#timeline',
 
@@ -25,19 +25,19 @@ var HistoryView = Backbone.View.extend({
   },
 
   updateLinks: function updateLinks(givenSection) {
-    var prefix = window.APP_PREFIX;
-    var section = givenSection;
+    let prefix = window.APP_PREFIX;
+    let section = givenSection;
     if (typeof prefix !== 'undefined' && prefix.substr(prefix.length - 1) !== '/') {
       prefix += '/';
     }
         // section may not be defined (e.g. on the landing page)
     if (typeof section !== 'undefined') {
       this.$el.find('.version-link').each(function perLink() {
-        var $link = $(this);
+        const $link = $(this);
         $link.attr('href', prefix + section + '/' + $link.data('version'));
       });
       this.$el.find('.stop-button').each(function perButton() {
-        var $link = $(this);
+        const $link = $(this);
         /* Interpretations are split into "subterps" outside of diff view -
          * link to the first */
         if (section.indexOf('Interp') !== -1) {
@@ -54,12 +54,11 @@ var HistoryView = Backbone.View.extend({
       }
       // update diff dropdown
       this.$el.find('.select-content form').each(function perForm() {
-        var $form = $(this);
-        var actionParts;
-        var actionPath;
-
+        const $form = $(this);
         // form action = diff_redirect/section/version
-        actionParts = _.compact($form.attr('action').split('/'));
+        const actionParts = _.compact($form.attr('action').split('/'));
+        let actionPath = '';
+
         // remove section ID
         actionParts.splice(-2, 1, section);
         actionPath = '/' + actionParts.join('/');

regulations/static/regulations/js/source/views/main/preamble-view.js
@@ -1,21 +1,21 @@
 
 
-var $ = require('jquery');
-var Backbone = require('backbone');
+const $ = require('jquery');
+const Backbone = require('backbone');
 
 Backbone.$ = $;
 
-var ChildView = require('./child-view');
-var MainEvents = require('../../events/main-events');
-var PreambleHeadView = require('../header/preamble-head-view');
-var CommentView = require('../comment/comment-view');
-var CommentIndexView = require('../comment/comment-index-view');
-var CommentEvents = require('../../events/comment-events');
-var DrawerEvents = require('../../events/drawer-events');
-var starsHelpers = require('./stars-helpers');
-var helpers = require('../../helpers');
+const ChildView = require('./child-view');
+const MainEvents = require('../../events/main-events');
+const PreambleHeadView = require('../header/preamble-head-view');
+const CommentView = require('../comment/comment-view');
+const CommentIndexView = require('../comment/comment-index-view');
+const CommentEvents = require('../../events/comment-events');
+const DrawerEvents = require('../../events/drawer-events');
+const starsHelpers = require('./stars-helpers');
+const helpers = require('../../helpers');
 
-var PreambleView = ChildView.extend({
+const PreambleView = ChildView.extend({
   events: {
     'click .activate-write': 'handleWriteLink',
     'click .citation.internal': 'openCitation',
@@ -24,7 +24,7 @@ var PreambleView = ChildView.extend({
   initialize: function initialize(options) {
     this.options = options;
 
-    var parsed = helpers.parsePreambleId(this.options.id);
+    const parsed = helpers.parsePreambleId(this.options.id);
 
     this.options.scrollToId = parsed.hash;
 
@@ -48,12 +48,12 @@ var PreambleView = ChildView.extend({
   },
 
   openCitation: function openCitation(e) {
-    var $target = $(e.currentTarget);
-    var hash = $target.attr('href');
-    var id = $target.attr('data-section-id');
-    var options = {};
-    var type = this.options.type;
-    var section = helpers.parsePreambleCitationId(hash, type);
+    const $target = $(e.currentTarget);
+    const hash = $target.attr('href');
+    const id = $target.attr('data-section-id');
+    const options = {};
+    const type = this.options.type;
+    const section = helpers.parsePreambleCitationId(hash, type);
 
     if (id) {
       e.preventDefault();
@@ -74,9 +74,9 @@ var PreambleView = ChildView.extend({
   },
 
   handleWriteLink: function handleWriteLink(e) {
-    var $target = $(e.target);
-    var $dataTarget = $target.closest('.activate-write');
-    var $section = $target.closest('[data-permalink-section]');
+    const $target = $(e.target);
+    const $dataTarget = $target.closest('.activate-write');
+    const $section = $target.closest('[data-permalink-section]');
 
     this.write(
       $dataTarget.data('section'),
@@ -90,7 +90,7 @@ var PreambleView = ChildView.extend({
   },
 
   handleWriteTab: function handleWriteTab() {
-    var $section = $('#' + this.section);
+    const $section = $('#' + this.section);
 
     this.write(
       $section.find('.activate-write').data('section'),
@@ -124,7 +124,7 @@ var PreambleView = ChildView.extend({
   },
 
   write: function write(section, tocId, indexes, label, givenParent) {
-    var $parent = givenParent;
+    let $parent = givenParent;
     this.mode = 'write';
     this.checkCanWrite();
 
@@ -177,8 +177,8 @@ var PreambleView = ChildView.extend({
     });
 
     if (this.options.mode === 'write') {
-      var $parent = $('#' + this.options.scrollToId);
-      var indexes = $parent.data('indexes');
+      const $parent = $('#' + this.options.scrollToId);
+      const indexes = $parent.data('indexes');
 
       this.write(this.options.section, this.options.tocId, indexes, this.options.label, $parent);
     } else {
@@ -188,10 +188,10 @@ var PreambleView = ChildView.extend({
   },
 
   collapseStars: function collapseStars() {
-    var $expander;
+    let $expander;
     this.$el.find('li[data-stars]').each(function perEl(idx, elt) {
-      var $li = $(elt);
-      var starType = $li.data('stars');
+      const $li = $(elt);
+      const starType = $li.data('stars');
       $expander = starsHelpers[starType]($li, $expander);
     });
   },

eregs/regulations-site

regulations/static/regulations/js/source/views/breakaway/breakaway-view.js
@@ -1,17 +1,17 @@
 
 
-var $ = require('jquery');
-var _ = require('underscore');
-var Backbone = require('backbone');
-var SxS = require('./sxs-view');
-var Router = require('../../router');
-var BreakawayEvents = require('../../events/breakaway-events');
-var MainEvents = require('../../events/main-events');
-var SidebarEvents = require('../../events/sidebar-events');
+const $ = require('jquery');
+const _ = require('underscore');
+const Backbone = require('backbone');
+const SxS = require('./sxs-view');
+const Router = require('../../router');
+const BreakawayEvents = require('../../events/breakaway-events');
+const MainEvents = require('../../events/main-events');
+const SidebarEvents = require('../../events/sidebar-events');
 
 Backbone.$ = $;
 
-var BreakawayView = Backbone.View.extend({
+const BreakawayView = Backbone.View.extend({
   childViews: {},
 
   initialize: function initialize() {
@@ -19,7 +19,7 @@ var BreakawayView = Backbone.View.extend({
   },
 
   openSxS: function openSxS(initialContext) {
-    var context = $.extend({}, initialContext);
+    const context = $.extend({}, initialContext);
     context.url = context.regParagraph + '/' + context.docNumber + '?from_version=' + context.fromVersion;
 
     this.childViews.sxs = new SxS(context);
@@ -38,5 +38,5 @@ var BreakawayView = Backbone.View.extend({
   },
 });
 
-var breakaway = new BreakawayView();
+const breakaway = new BreakawayView();
 module.exports = breakaway;

regulations/static/regulations/js/source/views/drawer/drawer-view.js
@@ -1,16 +1,16 @@
 
 
-var $ = require('jquery');
-var Backbone = require('backbone');
-var TOCView = require('./toc-view');
-var HistoryView = require('./history-view');
-var SearchView = require('./search-view');
-var DrawerTabs = require('./drawer-tabs-view');
-var DrawerEvents = require('../../events/drawer-events');
+const $ = require('jquery');
+const Backbone = require('backbone');
+const TOCView = require('./toc-view');
+const HistoryView = require('./history-view');
+const SearchView = require('./search-view');
+const DrawerTabs = require('./drawer-tabs-view');
+const DrawerEvents = require('../../events/drawer-events');
 
 Backbone.$ = $;
 
-var DrawerView = Backbone.View.extend({
+const DrawerView = Backbone.View.extend({
   el: '#menu',
 
   initialize: function initialize(options) {
@@ -26,7 +26,7 @@ var DrawerView = Backbone.View.extend({
     this.childViews.search = new SearchView();
     this.childViews['drawer-tabs'] = new DrawerTabs({ forceOpen: options.forceOpen });
 
-    var $tocSecondary = $('#table-of-contents-secondary');
+    const $tocSecondary = $('#table-of-contents-secondary');
     if ($tocSecondary.length) {
       this.childViews['table-of-contents-secondary'] = new TOCView({ el: $tocSecondary });
     }
@@ -46,7 +46,7 @@ var DrawerView = Backbone.View.extend({
 
   // selectedId = page type or child view type
   setActivePane: function setActivePane(selectedId) {
-    var activeId = selectedId;
+    let activeId = selectedId;
     if (typeof this.childViews[activeId] === 'undefined') {
       activeId = this.pageTypeMap[activeId];
     }

regulations/static/regulations/js/source/views/drawer/history-view.js
@@ -1,13 +1,13 @@
 
 
-var $ = require('jquery');
-var _ = require('underscore');
-var Backbone = require('backbone');
-var MainEvents = require('../../events/main-events');
+const $ = require('jquery');
+const _ = require('underscore');
+const Backbone = require('backbone');
+const MainEvents = require('../../events/main-events');
 
 Backbone.$ = $;
 
-var HistoryView = Backbone.View.extend({
+const HistoryView = Backbone.View.extend({
 
   el: '#timeline',
 
@@ -25,19 +25,19 @@ var HistoryView = Backbone.View.extend({
   },
 
   updateLinks: function updateLinks(initialSection) {
-    var section = initialSection;
-    var prefix = window.APP_PREFIX;
+    let section = initialSection;
+    let prefix = window.APP_PREFIX;
     if (typeof prefix !== 'undefined' && prefix.substr(prefix.length - 1) !== '/') {
       prefix += '/';
     }
     // section may not be defined (e.g. on the landing page)
     if (typeof section !== 'undefined') {
       this.$el.find('.version-link').each(function perLink() {
-        var $link = $(this);
+        const $link = $(this);
         $link.attr('href', prefix + section + '/' + $link.data('version'));
       });
       this.$el.find('.stop-button').each(function perButton() {
-        var $link = $(this);
+        const $link = $(this);
         /* Interpretations are split into "subterps" outside of diff view -
          * link to the first */
         if (section.indexOf('Interp') !== -1) {
@@ -54,12 +54,11 @@ var HistoryView = Backbone.View.extend({
       }
       // update diff dropdown
       this.$el.find('.select-content form').each(function perForm() {
-        var $form = $(this);
-        var actionParts;
-        var actionPath;
-
+        const $form = $(this);
         // form action = diff_redirect/section/version
-        actionParts = _.compact($form.attr('action').split('/'));
+        const actionParts = _.compact($form.attr('action').split('/'));
+        let actionPath = '';
+
         // remove section ID
         actionParts.splice(-2, 1, section);
         actionPath = '/' + actionParts.join('/');

regulations/static/regulations/js/source/views/main/preamble-view.js
@@ -1,21 +1,21 @@
 
 
-var $ = require('jquery');
-var Backbone = require('backbone');
+const $ = require('jquery');
+const Backbone = require('backbone');
 
 Backbone.$ = $;
 
-var ChildView = require('./child-view');
-var MainEvents = require('../../events/main-events');
-var PreambleHeadView = require('../header/preamble-head-view');
-var CommentView = require('../comment/comment-view');
-var CommentIndexView = require('../comment/comment-index-view');
-var CommentEvents = require('../../events/comment-events');
-var DrawerEvents = require('../../events/drawer-events');
-var starsHelpers = require('./stars-helpers');
-var helpers = require('../../helpers');
+const ChildView = require('./child-view');
+const MainEvents = require('../../events/main-events');
+const PreambleHeadView = require('../header/preamble-head-view');
+const CommentView = require('../comment/comment-view');
+const CommentIndexView = require('../comment/comment-index-view');
+const CommentEvents = require('../../events/comment-events');
+const DrawerEvents = require('../../events/drawer-events');
+const starsHelpers = require('./stars-helpers');
+const helpers = require('../../helpers');
 
-var PreambleView = ChildView.extend({
+const PreambleView = ChildView.extend({
   events: {
     'click .activate-write': 'handleWriteLink',
     'click .citation.internal': 'openCitation',
@@ -24,7 +24,7 @@ var PreambleView = ChildView.extend({
   initialize: function initialize(options) {
     this.options = options;
 
-    var parsed = helpers.parsePreambleId(this.options.id);
+    const parsed = helpers.parsePreambleId(this.options.id);
 
     this.options.scrollToId = parsed.hash;
 
@@ -48,12 +48,12 @@ var PreambleView = ChildView.extend({
   },
 
   openCitation: function openCitation(e) {
-    var $target = $(e.currentTarget);
-    var hash = $target.attr('href');
-    var id = $target.attr('data-section-id');
-    var options = {};
-    var type = this.options.type;
-    var section = helpers.parsePreambleCitationId(hash, type);
+    const $target = $(e.currentTarget);
+    const hash = $target.attr('href');
+    const id = $target.attr('data-section-id');
+    const options = {};
+    const type = this.options.type;
+    const section = helpers.parsePreambleCitationId(hash, type);
 
     if (id) {
       e.preventDefault();
@@ -74,9 +74,9 @@ var PreambleView = ChildView.extend({
   },
 
   handleWriteLink: function handleWriteLink(e) {
-    var $target = $(e.target);
-    var $dataTarget = $target.closest('.activate-write');
-    var $section = $target.closest('[data-permalink-section]');
+    const $target = $(e.target);
+    const $dataTarget = $target.closest('.activate-write');
+    const $section = $target.closest('[data-permalink-section]');
 
     this.write(
       $dataTarget.data('section'),
@@ -90,7 +90,7 @@ var PreambleView = ChildView.extend({
   },
 
   handleWriteTab: function handleWriteTab() {
-    var $section = $('#' + this.section);
+    const $section = $('#' + this.section);
 
     this.write(
       $section.find('.activate-write').data('section'),
@@ -124,7 +124,7 @@ var PreambleView = ChildView.extend({
   },
 
   write: function write(section, tocId, indexes, label, parentEl) {
-    var $parent = parentEl;
+    let $parent = parentEl;
     this.mode = 'write';
     this.checkCanWrite();
 
@@ -177,8 +177,8 @@ var PreambleView = ChildView.extend({
     });
 
     if (this.options.mode === 'write') {
-      var $parent = $('#' + this.options.scrollToId);
-      var indexes = $parent.data('indexes');
+      const $parent = $('#' + this.options.scrollToId);
+      const indexes = $parent.data('indexes');
 
       this.write(this.options.section, this.options.tocId, indexes, this.options.label, $parent);
     } else {
@@ -188,10 +188,10 @@ var PreambleView = ChildView.extend({
   },
 
   collapseStars: function collapseStars() {
-    var $expander;
+    let $expander;
     this.$el.find('li[data-stars]').each(function perEl(idx, elt) {
-      var $li = $(elt);
-      var starType = $li.data('stars');
+      const $li = $(elt);
+      const starType = $li.data('stars');
       $expander = starsHelpers[starType]($li, $expander);
     });
   },