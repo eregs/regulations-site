2e434d6e120193e1eefe1a9ca61d55aeea4ee111
CM Lubinski <cm.lubinski@gmail.com>
2016-12-31 11:48:54 -0500

    Bump to 7.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="regulations",
-    version="6.2.0",
+    version="7.0.0",
     packages=find_packages(),
     install_requires=[
         'boto3',
