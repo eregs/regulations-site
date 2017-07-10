f9a33b1518138e6cb3ffbb06fd6fe4e02236d10b
CM Lubinski <christopher.lubinski@gsa.gov>
2017-07-10 17:27:47 -0400

    Integrate tox with Travis.

--- integration-tests.sh
+++ integration-tests.sh
@@ -9,5 +9,5 @@ cp local_settings_test.py local_settings.py
 ./run_server.sh &
 sleep 5
 export UITESTS_URL=http://localhost:8000
-UITESTS_REMOTE=chrome py.test regulations/uitests -s
-UITESTS_REMOTE=ie11 py.test regulations/uitests -s
+UITESTS_REMOTE=chrome tox -e integration
+UITESTS_REMOTE=ie11 tox -e integration
