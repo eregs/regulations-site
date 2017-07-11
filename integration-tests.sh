07008968c0d0f2e903ada78a0c93d2c4d67e6fbe
CM Lubinski <christopher.lubinski@gsa.gov>
2017-07-11 10:01:12 -0400

    Use the Dockerized app in integration tests.
    This ensures a clear distinction between the application environment (now
    contained within Docker) and the testing environment (tox's "integration"
    env).

--- integration-tests.sh
+++ integration-tests.sh
@@ -6,7 +6,8 @@ cd dummy_api
 ./start.sh
 cd ..
 cp local_settings_test.py local_settings.py
-./run_server.sh &
+docker build . -t eregs/site
+docker run --rm -p 8000:8000 -d eregs/site
 sleep 5
 export UITESTS_URL=http://localhost:8000
 UITESTS_REMOTE=chrome tox -e integration
