f17b559e40f590fed7382664ac9eea47a8871eed
CM Lubinski <cm.lubinski@gmail.com>
2017-07-14 10:33:04 -0400

    Bump version to 7.1.0.
    We'd added support for Django 1.10 and 1.11 but continue to support 1.8 and
    1.9, so we'll just bump the minor version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="regulations",
-    version="7.0.2",
+    version="7.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
