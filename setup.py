504c7b3773c0f88ca405203b61c3d2ec384d7dcd
CM Lubinski <cm.lubinski@gmail.com>
2017-07-26 10:04:10 -0400

    Update version to 8.0.0.
    In this release:
    
    Some of the Notice-and-Comment functionality has been split into a separate
    Django app. This should significantly reduce the dependencies of down-stream
    applications which don't use that feature.
    
    Warning of data loss:
    
    The "FailedCommentSubmission" model has moved from `regulations` to
    `notice_comment` without a corresponding data migration. If this data is
    important, please export it before running migrations. Alternatively, you may
    run only the `notice_comment` migrations, manually transfer the data, then run
    the `regulations` migrations.
    
    Upgrade steps:
    
    If your app does not need Notice-and-Comment, no changes are needed.
    
    If your app does include N&C, you will need to:
    
    * Add `regulations[notice_comment]` as a Python requirement (rather than just
      `regulations`)
    * Add `notice_comment` to your `INSTALLED_APPS` in the Django settings
    * Mount the N&C urls either by directly including `notice_comment.urls` or by
      deferring to `regulations.all_urls`
    * Migrate your database. Note the potential for data loss above!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="regulations",
-    version="7.1.0",
+    version="8.0.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
