9a95fa2696313ca677761836fec6ed3d16d3acfb
CM Lubinski <cm.lubinski@gmail.com>
2017-07-26 10:03:14 -0400

    Rename urls file to indicate general use.

--- regulations/settings/dev.py
+++ regulations/settings/dev.py
@@ -6,7 +6,7 @@ CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
 CACHES['eregs_longterm_cache']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
 CACHES['api_cache']['TIMEOUT'] = 5  # roughly per request
 
-ROOT_URLCONF = 'regulations.test_urls'
+ROOT_URLCONF = 'regulations.all_urls'
 INSTALLED_APPS = INSTALLED_APPS + ('notice_comment',)
 
 try:
