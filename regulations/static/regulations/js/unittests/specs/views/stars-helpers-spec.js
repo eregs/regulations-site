bce9c17ea32557847934575975620477571f6074
CM Lubinski <christopher.lubinski@gsa.gov>
2017-08-15 12:30:17 -0400

    Update to jQuery 1.12.4.
    We'll probably want to switch to the 3.X line, but I didn't want to make that
    potentially breaking change now. 1.12.4's version of :visible doesn't work
    with DOM elements that aren't attached (including those of jsdom), so some
    tests needed to be tweaked to avoid that.

--- regulations/static/regulations/js/unittests/specs/views/stars-helpers-spec.js
+++ regulations/static/regulations/js/unittests/specs/views/stars-helpers-spec.js
@@ -2,45 +2,45 @@ var chai = require('chai');
 var expect = chai.expect;
 var jsdom = require('mocha-jsdom');
 
+let $;    // Must be loaded after the jsdom
+
+// jsdom doesn't support jQuery's latest revision of :visible
+function isVisible($el) {
+  const elHidden = $el.css('display') === 'none';
+  const parentHidden = $el.parents().filter(
+    (idx, p) => $(p).css('display') === 'none').length > 0;
+  return !elHidden && !parentHidden;
+}
+
 describe('starsHelpers.inline', function() {
   jsdom();
-  var starsHelpers, $;
+  var starsHelpers, $li;
 
   before(function() {
     $ = require('jquery');
     starsHelpers = require('../../../source/views/main/stars-helpers');
+    $li = $(
+      '<li><p>' +
+      '<span class="collapsed-marker">b.</span>' +
+      '<span class="paragraph-text">Remainder of the text</span>' +
+      '</p></li>');
   });
 
   it('does not hide the marker, but does hide the intro text', function() {
-    var $li = $(
-      '<li><p>' +
-      '<span class="collapsed-marker">b.</span>' +
-      '<span class="paragraph-text">Remainder of the text</span>' +
-      '</p></li>');
     starsHelpers.inline($li);
-    expect($li.find('.collapsed-marker').is(':visible')).to.be.true;
-    expect($li.find('.paragraph-text').is(':visible')).to.be.false;
+    expect(isVisible($li.find('.collapsed-marker'))).to.be.true;
+    expect(isVisible($li.find('.paragraph-text'))).to.be.false;
   });
   it('generates an expander which reverses the hiding', function() {
-    var $li = $(
-      '<li><p>' +
-      '<span class="collapsed-marker">b.</span>' +
-      '<span class="paragraph-text">Remainder of the text</span>' +
-      '</p></li>');
     var $expander = starsHelpers.inline($li);
     expect($li.find($expander)).to.be.ok;
 
     $expander.click();
-    expect($li.find('.collapsed-marker').is(':visible')).to.be.true;
-    expect($li.find('.paragraph-text').is(':visible')).to.be.true;
+    expect(isVisible($li.find('.collapsed-marker'))).to.be.true;
+    expect(isVisible($li.find('.paragraph-text'))).to.be.true;
     expect($li.find($expander).length).to.equal(0);
   });
   it('unsets and resets the "collapsed" class', function() {
-    var $li = $(
-      '<li><p class="collapsed">' +
-      '<span class="collapsed-marker">b.</span>' +
-      '<span class="paragraph-text">Remainder of the text</span>' +
-      '</p></li>');
     var $expander = starsHelpers.inline($li);
     expect($li.find('.collapsed')).not.to.be.truthy;
 
@@ -51,53 +51,43 @@ describe('starsHelpers.inline', function() {
 
 describe('starsHelpers.full', function() {
   jsdom();
-  var starsHelpers, $;
+  var starsHelpers, $li;
 
   before(function() {
     $ = require('jquery');
     starsHelpers = require('../../../source/views/main/stars-helpers');
+    $li = $(
+      '<li><p>' +
+      '<span class="collapsed-marker">b.</span>' +
+      '<span class="paragraph-text">Remainder of the text</span>' +
+      '</p></li>');
   });
 
   it('hides everything within the li if no expander is provided', function() {
-    var $li = $(
-      '<li><p>' +
-      '<span class="collapsed-marker">b.</span>' +
-      '<span class="paragraph-text">Remainder of the text</span>' +
-      '</p></li>');
     starsHelpers.full($li, null);
-    expect($li.is(':visible')).to.be.true;
-    expect($li.find('.collapsed-marker').is(':visible')).to.be.false;
-    expect($li.find('.paragraph-text').is(':visible')).to.be.false;
+    expect(isVisible($li)).to.be.true;
+    expect(isVisible($li.find('.collapsed-marker'))).to.be.false;
+    expect(isVisible($li.find('.paragraph-text'))).to.be.false;
   });
   it('if no expander is provided, a new one is generated', function() {
-    var $li = $(
-      '<li><p>' +
-      '<span class="collapsed-marker">b.</span>' +
-      '<span class="paragraph-text">Remainder of the text</span>' +
-      '</p></li>');
     var $expander = starsHelpers.full($li, null);
     expect($li.find($expander)).to.be.ok;
 
     $expander.click();
-    expect($li.find('.collapsed-marker').is(':visible')).to.be.true;
-    expect($li.find('.paragraph-text').is(':visible')).to.be.true;
+    expect(isVisible($li.find('.collapsed-marker'))).to.be.true;
+    expect(isVisible($li.find('.paragraph-text'))).to.be.true;
     expect($li.find($expander).length).to.equal(0);
   });
   it('reuses an existing expander, if provided and hides the li', function() {
-    var $li = $(
-      '<li><p class="collapsed">' +
-      '<span class="collapsed-marker">b.</span>' +
-      '<span class="paragraph-text">Remainder of the text</span>' +
-      '</p></li>');
     var $expander = $('<button>');
     starsHelpers.full($li, $expander);
 
-    expect($li.is(':visible')).to.be.false;
-    expect($li.find('.collapsed-marker').is(':visible')).to.be.false;
-    expect($li.find('.paragraph-text').is(':visible')).to.be.false;
+    expect(isVisible($li)).to.be.false;
+    expect(isVisible($li.find('.collapsed-marker'))).to.be.false;
+    expect(isVisible($li.find('.paragraph-text'))).to.be.false;
     $expander.click();
-    expect($li.is(':visible')).to.be.true;
-    expect($li.find('.collapsed-marker').is(':visible')).to.be.true;
-    expect($li.find('.paragraph-text').is(':visible')).to.be.true;
+    expect(isVisible($li)).to.be.true;
+    expect(isVisible($li.find('.collapsed-marker'))).to.be.true;
+    expect(isVisible($li.find('.paragraph-text'))).to.be.true;
   });
 });
