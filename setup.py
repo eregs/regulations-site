8b7cbed44195b4f05870a5cf1075de589f0f16fb
CM Lubinski <christopher.lubinski@gsa.gov>
2017-10-10 17:46:46 -0400

    Bump to 8.4.1.
    Bugfixes
    * Return missing footnotes #512

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="regulations",
-    version="8.4.0",
+    version="8.4.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
