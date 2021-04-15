88009868e0d2784c1f051988d526951ee8b9c8f3
Alex Bradley <alex.robert.bradley@gmail.com>
2021-04-15 11:48:43 -0400

    Added js functionality to have select move to version page

--- regulations/static/regulations/js/main.js
+++ regulations/static/regulations/js/main.js
@@ -9,12 +9,32 @@ function makeStateful(el) {
     }
 }
 
+function goToVersion() {
+    const select = document.querySelector("#view-options");
+    const options = document.querySelectorAll("#view-options [data-url]");
+
+    select.addEventListener('change', function() {
+        location.href  = this.options[this.selectedIndex].dataset.url;
+    });
+
+    for(const option of options) {
+        const url = option.dataset.url;
+        if(location.href.includes(url)) {
+            option.setAttribute("selected", "");
+            break;
+        }
+    }
+}
+
 function main() {
     const stateful_elements = document.querySelectorAll("[data-state]")
 
     for (const el of stateful_elements) {
         makeStateful(el);
     }
+
 }
 
 main();
+goToVersion();
+
