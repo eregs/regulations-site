6cc1e4a214673601a5157bdb81aaf3d298a03dd6
David Gage <whytheplatypus@gmail.com>
2021-04-28 17:40:05 -0400

    Initial vue and storybook install (#33)
    * Initial vue and storybook install
    
    * Add example for a RelatedRule component

---
+++ regulations/static/components/RelatedRule.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2><a :href="html_url">{{ title }}</a></h2>
+    <p>{{ abstract }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'related-rule',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    html_url: {
+      type: String,
+      required: true,
+    },
+    abstract: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {},
+
+  methods: {},
+};
+</script>
