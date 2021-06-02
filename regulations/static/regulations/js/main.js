4c96f2bab2188fa83ea395478fd8c783d7be6ddd
David Gage <whytheplatypus@gmail.com>
2021-06-02 11:39:42 -0400

    Set an active state on current toc link element (#49)

--- regulations/static/regulations/js/main.js
+++ regulations/static/regulations/js/main.js
@@ -1,6 +1,43 @@
 import RelatedRules from './RelatedRules.js';
 import Vue from "../../node_modules/vue/dist/vue.esm.browser.min.js";
 
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+function deactivateAllTOCLinks() {
+    const active_els = document.querySelectorAll(".menu-section.active");
+    for (let active_el of active_els) {
+        active_el.classList.remove("active");
+    }
+}
+
+function getCurrentSectionFromHash() {
+    const hash = window.location.hash.substring(1);
+    const citations = hash.split("-");
+    return citations.slice(0, 2).join("-");
+}
+
+function activateTOCLink() {
+    deactivateAllTOCLinks();
+    const section = getCurrentSectionFromHash();
+
+    const el = document.querySelector(`[data-section-id='${section}']`);
+    if (!el) return;
+
+    el.classList.add("active");
+    if (!isElementInViewport(el)) {
+        el.scrollIntoView();
+    }
+}
+
 function makeStateful(el) {
     const state_change_target = el.getAttribute("data-state-name");
     const state_change_buttons = document.querySelectorAll(`[data-set-state][data-state-name='${state_change_target}']`);
@@ -19,6 +56,9 @@ function main() {
     for (const el of stateful_elements) {
         makeStateful(el);
     }
+
+    window.addEventListener("hashchange", activateTOCLink);
+    activateTOCLink();
 
     
     new Vue({
